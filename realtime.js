ce722cd29f96d55ca0df5ed2fc45073cd0db5811
handaaaaaa <44576007+handaaaaaa@users.noreply.github.com>
2019-06-12 23:08:53 +0900

    Update realtime.js

--- realtime.js
+++ realtime.js
@@ -1,6 +1,9 @@
-var nowTime = new Date(); //  現在日時を得る
-var nowHour = nowTime.getHours(); // 時を抜き出す
-var nowMin  = nowTime.getMinutes(); // 分を抜き出す
-var nowSec  = nowTime.getSeconds(); // 秒を抜き出す
-var msg = "現在時刻は、" + nowHour + ":" + nowMin + ":" + nowSec + " です。";
-document.getElementById("RealtimeClockArea").innerHTML = msg;
+function showClock1() {
+   var nowTime = new Date();
+   var nowHour = nowTime.getHours();
+   var nowMin  = nowTime.getMinutes();
+   var nowSec  = nowTime.getSeconds();
+   var msg = "現在時刻は、" + nowHour + ":" + nowMin + ":" + nowSec + " です。";
+   document.getElementById("RealtimeClockArea").innerHTML = msg;
+}
+setInterval('showClock1()',1000);
